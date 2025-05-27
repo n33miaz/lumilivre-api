56bdaf6bf28400d02f7e61b076f562f882ba5e69
jpshawty38 <contato.jpshawty@gmail.com>
2025-05-26 22:18:00 -0300

    update

--- src/main/resources/templates/StatusLivroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lumilivre.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.lumilivre.api.model.StatusLivroModel;
-
-public interface StatusLivroRepository extends CrudRepository<StatusLivroModel, Long> {
-    
-}
--- target/classes/templates/StatusEmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lumilivre.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.lumilivre.api.model.StatusEmprestimoModel;
-
-public interface StatusEmprestimoRepository extends CrudRepository<StatusEmprestimoModel, Long>{
-    
-}
