265f5770f8207d306323df3f7bd4c022e21d07d5
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2025-09-08 20:30:05 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/lumilivre/api/config/SecurityConfig.java
+++ src/main/java/br/com/lumilivre/api/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
         public UrlBasedCorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000", "https://lumi-livre-web.vercel.app")); 
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000", "https://lumilivre-web.onrender.com")); 
             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
             configuration.setAllowCredentials(true);
