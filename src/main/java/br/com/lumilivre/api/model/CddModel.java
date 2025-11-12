4dfeb85fbfd6c38a2e6ad4019f0026867854fb99
João Oliveira <contato.jpshawty@gmail.com>
2025-11-11 21:18:39 -0300

    update relatorios

--- src/main/java/br/com/lumilivre/api/model/CddModel.java
+++ src/main/java/br/com/lumilivre/api/model/CddModel.java
@@ -14,4 +14,16 @@ public class CddModel {
     @Id
     private String codigo;
     private String descricao;
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
--- src/main/java/br/com/lumilivre/api/model/GeneroModel.java
+++ src/main/java/br/com/lumilivre/api/model/GeneroModel.java
@@ -16,4 +16,22 @@ public class GeneroModel {
 
     @Column(nullable = false, unique = true, length = 100)
     private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+    
+    
 }
