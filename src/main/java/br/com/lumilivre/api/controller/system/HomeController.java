05a3d7a0749f787f2347471880b6c995af27d048
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2026-05-16 00:24:13 -0300

    chore: drop v1 controllers

--- src/main/java/br/com/lumilivre/api/controller/system/HomeController.java
+++ src/main/java/br/com/lumilivre/api/controller/system/HomeController.java
@@ -1,17 +1,18 @@
-package br.com.lumilivre.api.controller.v1.system;
+package br.com.lumilivre.api.controller.system;
 
-import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "0. Home")
 @SecurityRequirement(name = "bearerAuth")
-public class InicioController {
+public class HomeController {
 
     @GetMapping("/")
-    @Operation(summary = "Verifica o status da API", description = "Retorna uma mensagem simples indicando que a API está online.")
+    @Operation(summary = "Health check", description = "Returns a simple message indicating the API is online.")
     public String home() {
         return "API Lumilivre rodando com sucesso!";
     }
